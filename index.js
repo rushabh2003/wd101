3ea91387f48ea8015cf81646e469bd57496d3398
rushabh2003 <114856443+rushabh2003@users.noreply.github.com>
2023-08-08 15:44:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-  const currentDate = new Date();
+const currentDate = new Date();
 const minDate = new Date();
 const maxDate = new Date();
 
 minDate.setFullYear(currentDate.getFullYear() - 55);
-// minDate.setDate(minDate.getDate() + 1);
-maxDate.setFullYear(currentDate.getFullYear() - 18);
-// maxDate.setDate(maxDate.getDate() + 1);  
+maxDate.setFullYear(currentDate.getFullYear() - 18);  
 
 document.getElementById("dob").setAttribute("min", minDate.toISOString().split('T')[0]);
 document.getElementById("dob").setAttribute("max", maxDate.toISOString().split('T')[0]);
@@ -29,17 +27,17 @@ function displayData() {
     const passwords = JSON.parse(localStorage.getItem("Passwords"));
     const dobs = JSON.parse(localStorage.getItem("DOBs"));
 
-    const tableBody = document.querySelector("#tableData tbody");
-    // Clear existing table rows
+    const tableBody = document.querySelector("#datatable tbody");
+    
     tableBody.innerHTML = "";
 
     for (let i = 0; i < names.length; i++) {
-        const row = document.createElement("tr");
-        const nameCell = document.createElement("td");
-        const emailCell = document.createElement("td");
-        const passCell = document.createElement("td");
-        const dobCell = document.createElement("td");
-        const agreedCell = document.createElement("td");
+        let row = document.createElement("tr");
+        let nameCell = document.createElement("td");
+        let emailCell = document.createElement("td");
+        let passCell = document.createElement("td");
+        let dobCell = document.createElement("td");
+        let agreedCell = document.createElement("td");
 
         nameCell.textContent = names[i];
         emailCell.textContent = emails[i];
@@ -60,31 +58,28 @@ function displayData() {
 
 displayData();
 
-function Submit() {
-
+function SaveData() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const dob = document.getElementById("dob").value;
-
-    var old_n = JSON.parse(localStorage.getItem("Names"));
-    old_n.push(name);
-    localStorage.setItem("Names", JSON.stringify(old_n));
     
     var old_e = JSON.parse(localStorage.getItem("Emails"));
     old_e.push(email);
     localStorage.setItem("Emails", JSON.stringify(old_e));
-    
-    var old_p = JSON.parse(localStorage.getItem("Passwords"));
-    old_p.push(password);
-    localStorage.setItem("Passwords", JSON.stringify(old_p));
-    
+
     var old_d = JSON.parse(localStorage.getItem("DOBs"));
     old_d.push(dob);
     localStorage.setItem("DOBs", JSON.stringify(old_d));
-
-
-    // Clear input fields
+  
+    var old_p = JSON.parse(localStorage.getItem("Passwords"));
+    old_p.push(password);
+    localStorage.setItem("Passwords", JSON.stringify(old_p));
+  
+    var old_n = JSON.parse(localStorage.getItem("Names"));
+    old_n.push(name);
+    localStorage.setItem("Names", JSON.stringify(old_n));
+    
     document.getElementById("name").value = "";
     document.getElementById("email").value = "";
     document.getElementById("password").value = "";
